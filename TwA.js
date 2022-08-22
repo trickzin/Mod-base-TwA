3f34e1ea4f1fddbdd9f78ea686daa75e8c4df0c6
Trick <91226824+trickzin@users.noreply.github.com>
2022-08-22 12:16:38 -0300

    Add files via upload

---
+++ TwA.js
@@ -0,0 +1,29 @@
+{
+ "version":"3",
+ "title":"Primária base TwA by Trick",
+ "description":"
+    ✨ Mod base v1 ✨
+
+ > Versão : 2.22.11.83
+
+• Funções de Privacidade GB
+• Privacidade de Chamadas
+• Icone de Mensagem Apagada
+• Mudar cor do Icone de Mensagem Apagada
+• Icone de Transmissão
+• Paleta de Cor nos Status
+• Chat Direto
+• Reiniciar Mod
+• Modo DND
+• Clean Cache (10- e 11+)
+• Clean Database
+• Backup & Restore
+• Separar Grupo de Chats
+• Nota de Voz
+• Confirmar Envio de Stickers
+• Xml para botões
+• Source Yo
+ 
+ > Erros Corrigidos.",
+ "download":"https://youtube.com/c/Trickkk"
+}
